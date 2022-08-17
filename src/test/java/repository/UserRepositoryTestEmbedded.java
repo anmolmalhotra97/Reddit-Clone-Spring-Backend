6954a7a8a5f4113646c7c192e20db3f8a35a3796
Anmol Malhotra <anmolmalhotra97@gmail.com>
2022-08-17 03:29:27 -0400

    Added Test case for Data Access Layer using In-Memory DB H2

---
+++ src/test/java/repository/UserRepositoryTestEmbedded.java
@@ -0,0 +1,38 @@
+package repository;
+
+import com.example.springredditclone.model.User;
+import com.example.springredditclone.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.Instant;
+import java.util.Optional;
+
+//Provided by spring test framework
+@DataJpaTest
+@ActiveProfiles("test")
+public class UserRepositoryTestEmbedded {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void shouldSaveUser() {
+        User user = new User(null, "test user", "secret password", "user@email.com", Instant.now(), true);
+        User savedUser = userRepository.save(user);
+
+        Assertions.assertThat(savedUser).usingRecursiveComparison().ignoringFields("userId").isEqualTo(user);
+    }
+
+    @Test
+    @Sql("classpath:test-data.sql")
+    //uses a sql file to pre-populate the data and then comparisons are made on that basis
+    public void shouldSaveUsersThroughSqlFile() {
+        Optional<User> test = userRepository.findByUserName("testuser_sql");
+        Assertions.assertThat(test).isNotEmpty();
+    }
+}
